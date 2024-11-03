d582aa7b8095da11e9102648ea77d21a36feaeb4
sohailuddinsyed <syedsohail601@gmail.com>
2024-11-03 14:56:47 -0800

    Complete approximate inaccessible nodes

--- src/extraction/__init__.py
+++ src/extraction/__init__.py
@@ -1,10 +1,12 @@
 from .algorithm1 import ExtractionAlgorithm
+from .algorithm2 import ApproximateInaccessibleNodes
 from .fidelity import FidelityMeasure
 from .victim_api import VictimAPI, load_victim_model
 from .graph_sampler import GraphSampler
 
 __all__ = [
     'ExtractionAlgorithm',
+    'ApproximateInaccessibleNodes',
     'FidelityMeasure',
     'VictimAPI',
     'load_victim_model',
